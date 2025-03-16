bb222f1b5995be6d4b957b9df9f3ae105950a286
johnb08 <johnb08.kodehode@gmail.com>
2025-03-16 14:34:21 +0100

    fist stage

--- Program.cs
+++ Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        //Navngi variabler:
+
+        //Vi bruker camelCase for å navni variabler som bare skal eksistere i scopet vi jobber i.
+
+        //Som regel trenger vi bare definere datatypen på en side av deklarasjonstegnet:
+        int num = 23;
+
+        //Det er også gyldig å skrive følgende:
+        var number = 42;
+        //Men da må vi stole på at datamaskinen implicit velger korrekt datatype fra verdien på venstre siden.
+        //I tilfellet over, velger datamaskinen at 42 er en integer. 
+
+        //Det samme gjelder når vi definerer kolleksjoner. 
+        //Så lenge datatypen kan trygt hentes fra høyre siden av deklarasjonstegnet,
+        //Er det helt trygt å bruke var på venstre siden.
+        var numList = new List<int>();
+        var arr = new int[3];
+
+
+        //Men hvis vi skal definere en statisk kolleksjon med forhåndsdefinerte verdier,
+        //Er det annbefalt fra Microsoft å definere type på venstre side av definisjonstegnet,
+        //Så bruke kolleksjonsekpressionen [] for å definere startverdiene av kolleksjonen.
+        List<string> names = ["Anders", "John", "Kevin", "Håkon"];
+
+        //Legg merke til at dette kan skape problemer hvis du definerer arrays:
+        int[] numArr = [3];
+        //I motsettning til arr på linje 23, har vi her definert et array på lengde 1,
+        //hvor numArr[0] = 3, 
+        //mens på linje 23, definerer vi et array på lengde 3, med default verdier for ints
+        //i hver posisjon. 
     }
 }
